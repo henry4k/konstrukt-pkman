c4492d4129fb769779684327471b5196c243081b
Henry Kielmann <henrykielmann@gmail.com>
2016-05-06 11:50:26 +0200

    Moved CLI to separate directory

---
+++ packagemanager-cli/main.lua
@@ -0,0 +1,60 @@
+local argparse = require 'argparse'
+local utils = require 'packagemanager-cli/utils'
+local Repository = require 'packagemanager/repository'
+local PackageIndex = require 'packagemanager/packageindex'
+
+local function Run( commands )
+    local parser = argparse()
+    parser:command_target('command')
+    for commandName, command in pairs(commands) do
+        local subparser = parser:command(commandName, command.description)
+        if command.setupParser then
+            command.setupParser(subparser)
+        end
+    end
+    local arguments = parser:parse()
+    commands[arguments.command].execute(arguments)
+end
+
+local commands = {}
+commands.list =
+{
+    description = 'Print packages.',
+
+    execute = function()
+        local index = utils.buildPackageIndex{localPackages=true, remotePackages=true}
+        utils.markUserRequirements(index)
+        for package in PackageIndex.packages(index) do
+            local status = utils.getPackageInstallationStatus(package)
+            print(string.format('%s %s %s', package.name, package.version, status))
+        end
+    end
+}
+commands.update =
+{
+    description = 'Synchronize package lists, install stuff.',
+
+    execute = function()
+        utils.updateRepos()
+        local index = utils.buildPackageIndex{localPackages=true, remotePackages=true}
+        utils.installRequirements(index)
+    end
+}
+commands.index =
+{
+    description = 'Generate a package index.  Needed for repositories.',
+
+    setupParser = function( parser )
+        parser:argument('file', 'Index location', 'index.json')
+            :args(1)
+        parser:argument('baseUrl', '')
+            :args(1)
+    end,
+
+    execute = function( arguments )
+        local index = utils.buildPackageIndex{localPackages=true}
+        Repository.saveIndexToFile(index, arguments.file, arguments.baseUrl)
+    end
+}
+
+Run(commands)
